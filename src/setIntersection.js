1faa5d6802e3b926655c8409aedcd4eda0c33174
TrevorJamesH <jetsamrover@gmail.com>
2017-02-07 14:09:36 -0800

    Adds setIntersection and tests

---
+++ src/setIntersection.js
@@ -0,0 +1,15 @@
+export default function setInstersection(array, intersect){
+  let result = []
+  for(let i=0;i<array.length;i++){
+    let intersection = false
+    for(let j=0;j<intersect.length;j++){
+      if(array[i] === intersect[j]){
+        intersection = true
+      }
+    }
+    if(intersection === true){
+      result.push(array[i])
+    }
+  }
+  return result
+}
--- test/factorial.js
+++ test/factorial.js
@@ -1,33 +1,35 @@
 import { expect } from 'chai'
-import factorial from '../src/factorial'
+import setIntersection from '../src/setIntersection'
 
-describe.only('factorial()', function(){
+describe.only('setIntersection()', function(){
 
   it('should be a function', function(){
-    expect(factorial).to.be.a('function')
+    expect(setIntersection).to.be.a('function')
   })
 
-  context("when given 7", function(){
-    it("should return 5040", function(){
-      expect(factorial(7)).to.equal(5040)
+  context("when given [1,2,3,4] and [3,4,5,6]", function(){
+    it("should return [3,4]", function(){
+      expect(setIntersection([1,2,3,4], [3,4,5,6])).to.eql([3,4])
     })
   })
 
-  context("when given 10", function(){
-    it("should return 3628800", function(){
-      expect(factorial(10)).to.equal(3628800)
+  context("when given [5,4,3,2,1] and [2,3,4,5,6]", function(){
+    it("should return [5,4,3,2]", function(){
+      expect(setIntersection([5,4,3,2,1], [2,3,4,5,6])).to.eql([5,4,3,2])
     })
   })
 
-  context("when given 0", function(){
-    it("should return 1", function(){
-      expect(factorial(0)).to.equal(1)
+  context("when given [1,2,3,4] and [9,7,0]", function(){
+    it("should return []", function(){
+      expect(setIntersection([1,2,3,4], [9,7,0])).to.eql([])
     })
   })
 
-  context("when given -1", function(){
-    it("should return undefined", function(){
-      expect(factorial(-1)).to.equal(undefined)
+  context("when given [1,2,3,4] and []", function(){
+    it("should return []", function(){
+      expect(setIntersection([1,2,3,4], [])).to.eql([])
     })
   })
+
+
 })
