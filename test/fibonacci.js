ce7fc40ed9c5231b327265f50c07aaf31f01c748
TrevorJamesH <jetsamrover@gmail.com>
2017-02-06 16:58:59 -0800

    Built fibonacci and passing test

---
+++ test/fibonacci.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai'
+import fibonacci from '../src/fibonacci'
+
+describe.only('fibonacci()', function(){
+
+  it('should be a function', function(){
+    expect(fibonacci).to.be.a('function')
+  })
+
+  it('should return an array of length 50, whos 9th value is 34', function(){
+    const fib = fibonacci(50)
+    expect(fib).to.be.an('array')
+    expect(fib.length).to.equal(50)
+    expect(fib[9]).to.equal(34)
+  })
+})
