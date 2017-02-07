3b606265aa2acbccf6f9495d9f74bc7b9aebfc5e
TrevorJamesH <jetsamrover@gmail.com>
2017-02-07 11:17:08 -0800

    Adds isPalindrome and tests

---
+++ test/isPalindrome.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai'
+import isPalindrome from '../src/isPalindrome'
+
+describe.only('isPalindrome()', function(){
+
+  it('should be a function', function(){
+    expect(isPalindrome).to.be.a('function')
+  })
+
+  context("Is given: radar", function(){
+    it('should return true', function(){
+      expect(isPalindrome('radar')).to.equal(true)
+    })
+  })
+
+  context("Is given: 123454321", function(){
+    it('should return true', function(){
+      expect(isPalindrome('123454321')).to.equal(true)
+    })
+  })
+
+  context("Is given: A man, a plan, a canal: Panama", function(){
+    it('should return true', function(){
+      expect(isPalindrome('A man, a plan, a canal: Panama')).to.equal(true)
+    })
+  })
+
+  context("Is given: boat", function(){
+    it('should return false', function(){
+      expect(isPalindrome('boat')).to.equal(false)
+    })
+  })
+
+  context("Is given: Palindrome", function(){
+    it('should return false', function(){
+      expect(isPalindrome('Palindrome')).to.equal(false)
+    })
+  })
+})
